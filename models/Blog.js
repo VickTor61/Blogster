f7c6c7681a1947117b24610886a17d350f0467c5
Enoma Victor Osasenaga <vickjunior2002@gmail.com>
2023-10-27 16:16:04 +0100

    Refactor date

--- models/Blog.js
+++ models/Blog.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 const blogSchema = new Schema({
   title: String,
   content: String,
-  createdAt: { type: Date, default: Date.now },
+  createdAt: { type: Date, default: Date.now() },
   _user: { type: Schema.Types.ObjectId, ref: 'User' }
 });
 
